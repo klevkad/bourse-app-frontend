b9dc630892b082b6e5c05702aff7616f201516b8
klevkad <koffi.ehuie@gmail.com>
2026-03-18 10:44:20 +0000

    Save

--- pages/1_Dashboard.py
+++ pages/1_Dashboard.py
@@ -5,12 +5,10 @@ import pandas as pd
 import plotly.express as px
 from openai import OpenAI
 import matplotlib.pyplot as plt
-# from ..connexion_function import login, logout
 from shares.config import API_URL, USER_ID, PORTEFEUILLES
 from shares.connexion_function import logout
-
-# import requests
 from bs4 import BeautifulSoup
+
 
 if "authenticated" not in st.session_state or not st.session_state["authenticated"]:
     st.warning("Veuillez vous connecter sur la page d'accueil.")
@@ -22,47 +20,6 @@ else:
 user_id = USER_ID
 portefeuille_list = st.session_state["portefeuilles"] if "portefeuilles" in st.session_state else []
 
-# # st.write(f"Utilisation de l'API Key: {'Définie' if OPENAI_API_KEY else 'Non définie'}")
-# if 'openai_api_key' not in st.session_state:
-#     st.session_state.openai_api_key = OPENAI_API_KEY
-# st.title("📊 Analyse générale du Portefeuille")
-
-#     # Vérification de la clé
-# if not OPENAI_API_KEY:
-#     st.error("❌ Veuillez entrer votre clé API OpenAI")
-#     st.info("Vous pouvez obtenir une clé sur : https://platform.openai.com/api-keys")
-#     st.stop()
-# else:
-#     st.success(f"✅ API Key configurée: {OPENAI_API_KEY[:8]}...")
-
-# instructions = (
-#         "Tu es analyste financier. On te donne une liste d'actions détenues dans un portefeuille. "
-#         "Pour chaque action à la BRVM, je veux que tu associes le dernier cours connu et la quantité détenue. "
-#         "Réponds uniquement à la question posée, sans inventer de données. "
-# )  
-# boutton pour actualiser les données (dernier_cours) de chaque action via chatgpt
-
-# def generate_summary(text, model="gpt-4o-mini"):
-#     try:
-#         client = OpenAI(api_key=OPENAI_API_KEY)
-        
-#         response = client.chat.completions.create(
-#             model=model,
-#             messages=[
-#                 {"role": "system", "content": instructions},
-#                 {"role": "user", "content": text}
-#             ],
-#             max_tokens=2000,
-#             temperature=0.1
-#         )
-        
-#         return response.choices[0].message.content
-        
-#     except Exception as e:
-#         st.error(f"❌ Erreur lors de la génération du résumé: {str(e)}")
-#         return None
-
-
 
 def extraire_table_bourse():
     # 1. Envoyer la requête à la page web
@@ -72,7 +29,6 @@ def extraire_table_bourse():
     if response.status_code == 200:
         # 2. Analyser le contenu HTML
         soup = BeautifulSoup(response.content, 'html.parser')
-        
         # 3. Trouver la table par son ID
         table = soup.find('table', {'class': 'table table-hover table-striped sticky-enabled'})
         if table:
@@ -93,43 +49,18 @@ def extraire_table_bourse():
         print(f"Erreur lors du chargement de la page : {response.status_code}")
         return None
 
-# Exemple d'utilisation
-
-# if st.button("Tcheck"):
-#     with st.spinner("Chargement"):
-#         url_bourse = "https://www.sikafinance.com/marches/aaz" # Remplacez par l'URL réelle
-#         df_quotes = extraire_table_bourse()
-#         if df_quotes is not None:
-#             print(df_quotes.head())
-#             st.write(f"Portefeuille sélectionné : {st.session_state['nom_selectionne']} (ID: {st.session_state["portefeuille_id"]})")
-
-  
-
-
-# if st.button("🔄 Actualiser les données du marché"):
-#     with st.spinner("Mise à jour des données via OpenAI..."):
-#         summary = generate_summary("Je veux la liste des actions à la BRVM et leur dernier cours connu.", model="gpt-4o-mini")
-#         if summary:
-#             st.success("✅ Données mises à jour avec succès !")
-#             st.text_area("Résumé des mises à jour :", summary, height=200)
 
 portefeuille_map = {p["nom_portefeuille"]: p["id"] for p in portefeuille_list}
-# with st.form("get_data_form"):
-#     with st.spinner("Chargement des portefeuilles..."):
-#         submit = st.form_submit_button("👆 Afficher l'analyse")
 
-#         if submit:
+if st.button("🔄 Actualiser les données"):
+    st.cache_data.clear()  # Vide le cache de fetch_all_data()
+    st.rerun()
 try:
     df_quotes = extraire_table_bourse()
     st.write(f"Portefeuille sélectionné : {st.session_state['nom_selectionne']} (ID: {st.session_state['portefeuille_id']})")
-        # Mettre à jour la table actions dans la BD avec les nouveaux cours
-        # response = requests.post(f"{API_URL}/actions/update_prices", json=df_quotes[['nom_entreprise','date_mise_a_jour', 'dernier_cours']].to_dict(orient='records'))
-        # if response.status_code == 200:
-        #     st.success("✅ Cours des actions mis à jour dans la base de données.")
-        # else:
-        #     st.error("❌ Échec de la mise à jour des cours dans la base de données.")
+  
 # 1. Récupération des données via l'API
-    @st.cache_data(ttl=60) # Cache pour éviter de surcharger l'API
+    @st.cache_data(ttl=300) # Cache pour éviter de surcharger l'API
     def fetch_all_data():
         trans = requests.get(f"{API_URL}/portefeuille/{st.session_state["portefeuille_id"]}/transactions").json()
         stocks = requests.get(f"{API_URL}/actions/").json()
@@ -230,14 +161,12 @@ try:
             hide_index=True
         ) 
         st.divider()
-        # df_quotes['Cours Clôture (FCFA)'] = df_quotes['Cours Clôture (FCFA)'].str.replace(' ','').astype(float)
         df_quotes[['Cours Ouverture (FCFA)','Cours veille (FCFA)','Cours Clôture (FCFA)','Volume']] = df_quotes[['Cours Ouverture (FCFA)','Cours veille (FCFA)','Cours Clôture (FCFA)','Volume']].replace(' ','', regex=True).astype(float)
         if df_quotes is not None:
             st.title("💹 Cours Actuels des Actions à la BRVM")
             # Ajouter le secteur si besoin
             if 'secteur' not in df_quotes.columns:
                 df_quotes = df_quotes.merge(df_stocks[['symbole','secteur']], left_on='Symbole', right_on='symbole', how='left').drop(columns=['symbole'])
-            # st.pyplot(px.bar(df_quotes, x='Symbole', y='Cours Clôture (FCFA)', title="Cours de Clôture des Actions à la BRVM").update_layout(yaxis_title='Cours Clôture (FCFA)', xaxis_title='Symbole'))
             # camembert des secteurs
             col1, col2 = st.columns(2)
             with col1:
@@ -277,46 +206,3 @@ try:
 except Exception as e:
     st.error(f"Erreur lors de la sélection du portefeuille.{str(e)}")
 
-
-# # Récupérer les transactions pour calculer le portefeuille
-# trans_res = requests.get(f"{API_URL}/users/{USER_ID}/transactions")
-# if trans_res.status_code == 200 and trans_res.json():
-#     df_t = pd.DataFrame(trans_res.json())
-    
-#     # Calculer les quantités nettes par action
-#     df_t['qty_signed'] = df_t.apply(lambda x: x['quantity'] if x['transaction_type'] == 'buy' else -x['quantity'], axis=1)
-#     portfolio = df_t.groupby('stock_id')['qty_signed'].sum().reset_index()
-#     portfolio = portfolio[portfolio['qty_signed'] > 0] # Garder seulement ce qu'on possède
-    
-#     # Fusionner avec les noms des stocks
-#     stocks_df = pd.DataFrame(stocks)
-#     portfolio = portfolio.merge(stocks_df, on='stock_id')
-    
-#     # Affichage
-#     col1, col2 = st.columns([2, 1])
-    
-#     with col1:
-#         st.dataframe(portfolio[['symbol', 'company_name', 'qty_signed', 'sector']], width='content')
-    
-#     with col2:
-#         fig = px.pie(portfolio, values='qty_signed', names='symbol', title="Répartition des titres", hole=0.4)
-#         st.plotly_chart(fig, width='content')
-# else:
-#     st.info("Aucune transaction enregistrée pour le moment.")
-
-
-# # Vérification de l'état de connexion
-# # if "authenticated" not in st.session_state or not st.session_state["authenticated"]:
-# #     login()
-# #     # On cache les autres pages en mode Sidebar si pas connecté
-# #     st.markdown("<style>ul[data-testid='main-menu-list'] {display: none;}</style>", unsafe_allow_html=True)
-# # else:
-# #     st.sidebar.success(f"Connecté : {st.session_state['username']}")
-# #     if st.sidebar.button("👆 Déconnexion"):
-# #         logout()
-    
-# #     st.title("🏠 Accueil Portefeuille")
-# #     st.write(f"Bonjour **{st.session_state['username']}**, utilisez le menu à gauche pour piloter vos investissements.")
-# #     st.info("Sélectionnez 'Dashboard' pour voir vos gains ou 'Trading' pour passer des ordres.")
-# # Récupérer les données via API en utilisant st.session_state["user_id"]
-# # ... (Insère ici ton code de graphiques précédent)
